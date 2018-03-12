1942c41434c0af60eca4514ef97fa26603d42ce8
Guillaume <guillaume.jouet-pastre@hotmail.fr>
2018-03-12 11:21:25 +0100

    Guy 1st commit

--- src/index.php
+++ src/index.php
@@ -1,5 +1,31 @@
-<?php
+<!doctype html>
+<html lang=fr>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Acceuil</title>
+</head>
+<body>
 
-echo "hello word"
+<header>
 
-?>
+    <div class ="logo">
+        <img src="../res/images/Logo.png" alt="Image du logo" class="logopng">
+        <img src="../res/images/KingDome.png" alt="Image du nom de l'application" class="kingdomepng">
+    </div>
+
+    <nav>
+        <button>Acceuil</button>
+        <button>Catalogue</button>
+        <button>Nous contacter</button>
+    </nav>
+
+</header>
+
+<div class="ariane">
+    <img src="../res/images/KingDome.png" alt="Image du nom de l'application" class="kingdomepng2">
+
+</div>
+
+</body>
+</html>
