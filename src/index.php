5d2f61693ec2f9e1c17aecf781f11194d67873c3
Guillaume <guillaume.jouet-pastre@hotmail.fr>
2018-03-13 10:40:12 +0100

    Guy 2nd commit

--- src/index.php
+++ src/index.php
@@ -7,28 +7,9 @@
 </head>
 <body>
 
-<header>
-
-    <div class="nom">
-        <img src="../res/images/KingDome.png" alt="Image du nom de l'application" class="kingdomepng" >
-    </div>
-
-    <div class="logo">
-        <img src="../res/images/Logo.png" alt="Image du logo" class="logopng">
-    </div>
-
-    <nav>
-        <button>Acceuil</button>
-        <button>Catalogue</button>
-        <button>Nous contacter</button>
-    </nav>
-
-</header>
-
-<div class="ariane">
-    <img src="../res/images/KingDome.png" alt="Image du nom de l'application" class="kingdomepng2">
-
-</div>
+<?php
+include 'php/header.php';
+?>
 
 
 </body>
---
+++ src/php/header.php
@@ -0,0 +1,22 @@
+<header>
+
+    <div class="nom">
+        <img src="../../res/images/KingDome.png" alt="Image du nom de l'application" class="kingdomepng" >
+    </div>
+
+    <div class="logo">
+        <img src="../../res/images/Logo.png" alt="Image du logo" class="logopng">
+    </div>
+
+    <nav>
+        <button>Acceuil</button>
+        <button>Catalogue</button>
+        <button>Nous contacter</button>
+    </nav>
+
+</header>
+
+<div class="ariane">
+    <img src="../../res/images/KingDome.png" alt="Image du nom de l'application" class="kingdomepng2">
+
+</div>
