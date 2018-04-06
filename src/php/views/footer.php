0dd6c200c2491761102c9562866083121c0c3f5d
jo <jo@172.16.234.218>
2018-04-06 10:25:22 +0200

    Footer.php has been changed

--- src/php/views/footer.php
+++ src/php/views/footer.php
@@ -15,7 +15,7 @@
                     
                     <div id="bouton">
                         <p>
-                            <button><a href="contact.php">Nous contacter !</a></button>
+                            <button><a href="Page nous contacter">Nous contacter !</a></button>
                         </p>
                     </div>
 
@@ -25,17 +25,10 @@
                     <h3>Rejoignez nous sur les réseaux sociaux !</h3>
 
                     <p>
-<<<<<<< HEAD
-                        <img src="/KingDome/res/icones/facebook.png" alt="Facebook" />
-                        <img src="/KingDome/res/icones/instagram.png" alt="Instagram" />
-                        <img src="/KingDome/res/icones/twitter.png" alt="twitter" />
-                        <img src="/KingDome/res/icones/linkedin.png" alt="linkedin" />
-=======
                         <img src="../../../res/icones/facebook.png" alt="Facebook" />
                         <img src="../../../res/icones/instagram.png" alt="Instagram" />
                         <img src="../../../res/icones/twitter.png" alt="twitter" />
                         <img src="../../../res/icones/linkedin.png" alt="linkedin" />
->>>>>>> d07f6f5f04d58c49ddef4c7a9a27308224ca9eef
                     </p>
                 </div>
 
