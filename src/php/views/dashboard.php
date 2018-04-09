168ccebc2d731b2fee3687463fc7786e036565d6
Olfa <Olfa@172.16.237.210>
2018-04-09 14:25:07 +0200

    d

--- src/php/views/dashboard.php
+++ src/php/views/dashboard.php
@@ -14,22 +14,13 @@
 <body>
 	<div id=corps>
 		<div id="afficher">
-	    	<div id="masquer">
-	    	
-	    			<div class="ecriture1">
-	    				
-	    				<a href="#afficher" class="afficher">Maison 1
-	    					<img class="fleche" src="..\res\icones\fleche_gauche.png">
-	    				</a>
-	    				<a href="#masquer" class="masquer">Maison 1
-	    					<img class="fleche" src="..\res\icones\fleche_bas.png">
-	    				</a>
-	    				
-	    			</div>
-	    					
-		   			<div id="conteneur">
-		   				<div class="element , ecriture2">Ajouter un capteur</div>
-	 					<div class="element">
+			<div class="ecriture1">
+				<img class="fleche" src="..\res\icones\fleche_bas.png">
+	    		Maison 1
+	    	</div>
+			<div id="conteneur">
+		   		<div class="element , ecriture2">Ajouter un capteur</div>
+	 			<div class="element">
 		   					
 		    				<span class="custom-dropdown custom-dropdown--white">
 		    				    <select class="custom-dropdown__select custom-dropdown__select--white">
@@ -39,12 +30,39 @@
 							   	</select>
 							</span>
 								
-						</div>
-	    			</div>
-	         		<p id="texte">eherhzeegr</p>
-	        					         			
-	      	</div>
-	  	 </div>
-	  </div>
+				</div>
+	    	</div>
+	        <div class="pieces">
+	        	<div class="piece">
+	        		Pièce 1
+	        	</div>
+	        	<div class="piece">
+	        		Pièce 2
+	        	</div>
+	        </div>
+	     </div>
+		<div id="afficher">
+			<div class="ecriture1">
+				<img class="fleche" src="..\res\icones\fleche_bas.png">
+	    		Maison 2
+	    	</div>
+			<div id="conteneur">
+		   		<div class="element , ecriture2">Ajouter un capteur</div>
+	 			<div class="element">
+		   					
+		    				<span class="custom-dropdown custom-dropdown--white">
+		    				    <select class="custom-dropdown__select custom-dropdown__select--white">
+							       	<option>Trier par : Pièce</option>
+							       	<option>Type</option>
+							       	<option>Favori</option>
+							   	</select>
+							</span>
+								
+				</div>
+	    	</div>
+	        <p id="texte">eherhzeegr</p>
+	     </div>
+	</div>
+	  	 
 </body>
 </html>
