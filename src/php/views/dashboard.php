2da16cdbe1609c9f90c216c22a808b8210a46a38
Olfa <Olfa@172.16.237.210>
2018-04-09 10:05:06 +0200

    dashboard.php et .css

---
+++ src/php/views/dashboard.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang=fr>
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="..\src\css\style.css">
+    <link rel="stylesheet" href="..\src\css\dashboard.css">
+    <title>Dashboard</title>
+</head>
+
+<body>
+	<div id=corps>
+		<div id="afficher">
+	    	<div id="masquer">
+	    	
+	    			<div class="ecriture1">
+	    				
+	    				<a href="#afficher" class="afficher">Maison 1
+	    					<img class="fleche" src="C:\Users\Olfa\Documents\html\site\WebContent\fleche_gauche.png">
+	    				</a>
+	    				<a href="#masquer" class="masquer">Maison 1
+	    					<img class="fleche" src="C:\Users\Olfa\Documents\html\site\WebContent\fleche_bas.png">
+	    				</a>
+	    				
+	    			</div>
+	    					
+		   			<div id="conteneur">
+		   				<div class="element , ecriture2">Ajouter un capteur</div>
+	 					<div class="element">
+		   					
+		    				<span class="custom-dropdown custom-dropdown--white">
+		    				    <select class="custom-dropdown__select custom-dropdown__select--white">
+							       	<option>Trier par : Pièce</option>
+							       	<option>Type</option>
+							       	<option>Favori</option>
+							   	</select>
+							</span>
+								
+						</div>
+	    			</div>
+	         		<p id="texte">eherhzeegr</p>
+	        					         			
+	      	</div>
+	  	 </div>
+	  </div>
+</body>
+</html>
