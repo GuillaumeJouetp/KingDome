d9c1be2d65a6ff040a01602d80086dead8b99a8e
Olfa <Olfa@DESKTOP-K59HCFE>
2018-04-30 11:11:39 +0200

    ajout fonction dashboard

--- src/php/controllers/dashboard.php
+++ src/php/controllers/dashboard.php
@@ -1,5 +1,33 @@
 <?php
 
+// si la fonction n'est pas définie ou est vide, on choisit d'afficher la vue par default
+if (!isset($_GET['function']) || empty($_GET['function'])) {
+	$function = "notdone";
+} else {
+	$function = $_GET['function'];
+}
+
+// si l'utilisateur est connecté alors on lui affiche la dashboard
+if(isUserConnected($bdd)) {
+	$vue = "dashboard";
+}
+else{
+	switch ($function) {
+		case 'notdone':
+			//si l'utilisateur n'est pas connecté alors on lui demande de se connecter
+			$vue = "dashboardSansCompte";
+			break;
+	
+			
+		default :
+			// si aucune fonction ne correspond au paramètre function passé en GET
+			$vue = "error404";
+			$title = "error404";
+	}
+}
+
+
 include "php/views/header.php";
-include('php/views/dashboard.php');
+include ('php/views/' . $vue . '.php');
 include "php/views/footer.php";
+?>
--- src/php/views/dashboard.php
+++ src/php/views/dashboard.php
@@ -13,7 +13,6 @@
 
 <body>
 	<div id=corps>
-		<div id="afficher">
 			<div class="ecriture1">
 				<img class="fleche" src="..\res\icones\fleche_bas.png">
 	    		Maison 1
@@ -58,51 +57,6 @@
 	    		</div>
 	        	
 	        </div>
-	     </div>
-		<div id="afficher">
-			<div class="ecriture1">
-				<img class="fleche" src="..\res\icones\fleche_bas.png">
-	    		Maison 2
-	    	</div>
-			<div id="conteneur">
-		   		<div class="element , ecriture2">Ajouter un capteur</div>
-	 			<div class="element">
-		   					
-		    				<span class="custom-dropdown custom-dropdown--white">
-		    				    <select class="custom-dropdown__select custom-dropdown__select--white">
-							       	<option>Trier par : Pièce</option>
-							       	<option>Type</option>
-							       	<option>Favori</option>
-							   	</select>
-							</span>
-								
-				</div>
-	    	</div>
-	        <div class="pieces">
-	        	<div class="piece">
-	        		Pièce 1
-	        	</div>
-	        	<div id="conteneur">
-	        	
-		   		<div class="element , ecriture3">Texte</div>
-	 			<div class="element , ecriture3">Texte</div>
-	 			<div class="element , ecriture3">Texte</div>
-	 			
-	    		</div>
-	        	
-	        	<div class="piece">
-	        		Pièce 2
-	        	</div>
-	        	
-	        	<div id="conteneur">
-	        	
-		   		<div class="element , ecriture3">Texte</div>
-	 			<div class="element , ecriture3">Texte</div>
-	 			<div class="element , ecriture3">Texte</div>
-	 			
-	    		</div>
-	        </div>
-	     </div>
 	     </div>
 	</div>
 	  	 
