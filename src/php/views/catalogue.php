7a30fa44e5bf7732e1a56c5a19cb31491db24737
LucasAPP92 <lucaslemal@gmail.com>
2018-05-02 09:50:12 +0200

    Catalogue et images

--- src/php/views/catalogue.php
+++ src/php/views/catalogue.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="Catalogue.css" />
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 
-    <title>Test</title>
+    <title>Te st</title>
 </head>
 
 <body>
