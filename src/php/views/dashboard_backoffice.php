b7045868b82e3efaa97b8013dca5eaf95a23fe28
Guillaume <guillaume.jouet-pastre@hotmail.fr>
2018-05-02 11:36:22 +0200

    test

--- src/php/views/dashboard_backoffice.php
+++ src/php/views/dashboard_backoffice.php
@@ -1,22 +1,6 @@
-<?php
-/**
- * VUE DE LA PAGE DASHBOARD
- * User: Olfa
- * Fini
- */
-?>
-
-
 <head>
     <link rel="stylesheet" href="..\src\css\dashboard.css">
 </head>
 
-<body>
 	<div id=corps>
-	
-			<h1> Admin </h1>
 
-	</div>
-	  	 
-</body>
-</html>
