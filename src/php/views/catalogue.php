910c7154eb9ddc9211617f29f7e49cd00d662d12
LucasAPP92 <lucaslemal@gmail.com>
2018-05-02 09:44:30 +0200

    Catalogue et images

---
+++ src/php/views/catalogue.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="Catalogue.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+
+    <title>Test</title>
+</head>
+
+<body>
+
+<h1>Catalogue</h1>
+
+<div class="centrer">Vous trouverez ici les différents capteurs vendus par DOMISEP ainsi que leurs caractéristiques.</div>
+<div class="centrer">Pour plus d'informations n'hésitez pas à nous contacter directement, vous trouverez nos coordonnées <span class="bleue">EN CLIQUANT ICI.</span></div>
+<div class="centrer"> <span class="rouge">ATTENTION : </span>Il ne s'agit pas d'une platforme de vente en ligne, vous ne pourrez donc pas acheter les capteurs directement sur le site.</div>
+
+<div class="trait_dessus"></div>
+
+<h2>Capteur de température</h2>
+
+<div id="carrousel">
+    <ul>
+        <li><img src="température/essai1.jpg" /></li>
+        <li><img src="température/essai2.jpg" /></li>
+        <li><img src="température/essai3.jpg" /></li>
+        <li><img src="température/essai4.jpg" /></li>
+    </ul>
+</div>
+
+<script src="Catalogue.js"></script>
+
+<div class="trait_dessus"></div>
+
+<h2>Capteur d'humidité</h2>
+
+<div id="carrousel2">
+    <ul>
+        <li><img src="humidité/essai1.jpg" /></li>
+        <li><img src="humidité/essai2.jpg" /></li>
+        <li><img src="humidité/essai3.jpg" /></li>
+    </ul>
+</div>
+
+<script src="Catalogue2.js"></script>
+
+<div class="trait_dessus"></div>
+
+<h2>Capteur de luminosité</h2>
+
+<div id="carrousel3">
+    <ul>
+        <li><img src="luminosité/essai1.jpg" /></li>
+        <li><img src="luminosité/essai2.jpg" /></li>
+        <li><img src="luminosité/essai3.jpg" /></li>
+        <li><img src="luminosité/essai4.jpg" /></li>
+        <li><img src="luminosité/essai5.jpg" /></li>
+        <li><img src="luminosité/essai6.jpg" /></li>
+    </ul>
+</div>
+
+<script src="Catalogue3.js"></script>
+
+</body>
+
+</html>
