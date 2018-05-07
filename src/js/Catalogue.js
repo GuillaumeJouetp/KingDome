a59c17fe7fa7b0c14d9627ed0f2b7be5587b80cc
LucasAPP92 <lucaslemal@gmail.com>
2018-05-07 15:08:25 +0200

    Catalogue

--- src/js/Catalogue.js
+++ src/js/Catalogue.js
@@ -9,26 +9,9 @@ $(document).ready(function() {
     $img.css('display', 'none');
     $currentImg.css('display', 'block');
 
-    $carrousel.append('<div class="controls"> <span class="prev"><img src="../res/icones/arrow-point-to-left.png"></span> <span class="next"><img src="../res/icones/arrow-point-to-right.png"></span> </div>');
+    $carrousel.append('<div class="controls"><span class="prev"><img src="../res/icones/arrow-point-to-left.png"></span><span class="next"><img src="../res/icones/arrow-point-to-right.png"></span></div>');
 
 
-    $('.next').click(function () {
-
-        i++;
-
-        if (i <= indexImg) {
-            $img.css('display', 'none');
-            $currentImg = $img.eq(i);
-            $currentImg.css('display', 'block');
-        }
-        else {
-            i = 0;
-            $img.css('display', 'none');
-            $currentImg = $img.eq(i);
-            $currentImg.css('display', 'block');
-        }
-
-    });
 
     $('.prev').click(function () {
 
@@ -48,5 +31,67 @@ $(document).ready(function() {
 
     });
 
+    $('.next').click(function () {
+
+        i++;
+
+        if (i <= indexImg) {
+            $img.css('display', 'none');
+            $currentImg = $img.eq(i);
+            $currentImg.css('display', 'block');
+        }
+        else {
+            i = 0;
+            $img.css('display', 'none');
+            $currentImg = $img.eq(i);
+            $currentImg.css('display', 'block');
+        }
+
+    });
+
 
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/php/views/catalogue.php
+++ src/php/views/catalogue.php
@@ -12,79 +12,79 @@
 
 <div id="corps">
 
-<h1>Catalogue</h1>
+    <h1>Catalogue</h1>
 
-<div class="centrer">Vous trouverez ici les différents capteurs vendus par DOMISEP.</div>
-<div class="centrer">Pour plus d'informations n'hésitez pas à nous contacter directement, vous trouverez nos coordonnées en bas de la page.</div>
-<div class="centrer"> <span class="rouge">ATTENTION : </span>Il ne s'agit pas d'une platforme de vente en ligne, vous ne pourrez donc pas acheter les capteurs directement sur le site.</div>
+    <div class="centrer">Vous trouverez ici les différents capteurs vendus par DOMISEP.</div>
+    <div class="centrer">Pour plus d'informations n'hésitez pas à nous contacter directement, vous trouverez nos coordonnées en bas de la page.</div>
+    <div class="centrer"> <span class="rouge">ATTENTION : </span>Il ne s'agit pas d'une platforme de vente en ligne, vous ne pourrez donc pas acheter les capteurs directement sur le site.</div>
 
-<div class="trait_dessus"></div>
+    <div class="trait_dessus"></div>
 
-<h2>Capteur de température</h2>
+    <h2>Capteur de température</h2>
 
-<div id="carrousel">
-    <ul>
-        <li><img src="../res/images/Catalogue/Température/Capteur1.png"  class="imgborder" /></li>
-        <li><img src="../res/images/Catalogue/Température/Capteur2.png"  class="imgborder" /></li>
-        <li><img src="../res/images/Catalogue/Température/Capteur3.png"  class="imgborder" /></li>
-    </ul>
-</div>
+    <div id="carrousel">
+        <ul>
+            <li><img src="../res/images/Catalogue/Température/Capteur1.png"  class="imgborder" /></li>
+            <li><img src="../res/images/Catalogue/Température/Capteur2.png"  class="imgborder" /></li>
+            <li><img src="../res/images/Catalogue/Température/Capteur3.png"  class="imgborder" /></li>
+        </ul>
+    </div>
 
-<script src="../src/js/Catalogue.js"></script>
+    <script src="../src/js/Catalogue.js"></script>
 
-<div class="trait_dessus"></div>
+    <div class="trait_dessus"></div>
 
-<h2>Capteur d'humidité</h2>
+    <h2>Capteur d'humidité</h2>
 
-<div id="carrousel2">
-    <ul>
-        <li><img src="../res/images/Catalogue/Humidité/Capteur1.png"  class="imgborder" /></li>
-        <li><img src="../res/images/Catalogue/Humidité/Capteur2.png"  class="imgborder" /></li>
-        <li><img src="../res/images/Catalogue/Humidité/Capteur3.png"  class="imgborder" /></li>
-    </ul>
-</div>
+    <div id="carrousel2">
+        <ul>
+            <li><img src="../res/images/Catalogue/Humidité/Capteur1.png"  class="imgborder" /></li>
+            <li><img src="../res/images/Catalogue/Humidité/Capteur2.png"  class="imgborder" /></li>
+            <li><img src="../res/images/Catalogue/Humidité/Capteur3.png"  class="imgborder" /></li>
+        </ul>
+    </div>
 
-<script src="../src/js/Catalogue2.js"></script>
+    <script src="../src/js/Catalogue2.js"></script>
 
-<div class="trait_dessus"></div>
+    <div class="trait_dessus"></div>
 
-<h2>Capteur de luminosité</h2>
+    <h2>Capteur de luminosité</h2>
 
-<div id="carrousel3">
-    <ul>
-        <li><img src="../res/images/Catalogue/Luminosité/Capteur1.png"  class="imgborder" /></li>
-        <li><img src="../res/images/Catalogue/Luminosité/Capteur2.png"  class="imgborder" /></li>
-        <li><img src="../res/images/Catalogue/Luminosité/Capteur3.png"  class="imgborder" /></li>
-    </ul>
-</div>
+    <div id="carrousel3">
+        <ul>
+            <li><img src="../res/images/Catalogue/Luminosité/Capteur1.png"  class="imgborder" /></li>
+            <li><img src="../res/images/Catalogue/Luminosité/Capteur2.png"  class="imgborder" /></li>
+            <li><img src="../res/images/Catalogue/Luminosité/Capteur3.png"  class="imgborder" /></li>
+        </ul>
+    </div>
 
-<script src="../src/js/Catalogue3.js"></script>
+    <script src="../src/js/Catalogue3.js"></script>
 
-<div class="trait_dessus"></div>
+    <div class="trait_dessus"></div>
 
-<h2>Capteur de fumée</h2>
+    <h2>Capteur de fumée</h2>
 
-<div id="carrousel4">
-    <ul>
-        <li><img src="../res/images/Catalogue/Fumée/Capteur1.png"  class="imgborder" /></li>
-        <li><img src="../res/images/Catalogue/Fumée/Capteur2.png"  class="imgborder" /></li>
-    </ul>
-</div>
+    <div id="carrousel4">
+        <ul>
+            <li><img src="../res/images/Catalogue/Fumée/Capteur1.png"  class="imgborder" /></li>
+            <li><img src="../res/images/Catalogue/Fumée/Capteur2.png"  class="imgborder" /></li>
+        </ul>
+    </div>
 
-<script src="../src/js/Catalogue4.js"></script>
+    <script src="../src/js/Catalogue4.js"></script>
 
-<div class="trait_dessus"></div>
+    <div class="trait_dessus"></div>
 
-<h2>Caméra</h2>
+    <h2>Caméra</h2>
 
-<div id="carrousel5">
-    <ul>
-        <li><img src="../res/images/Catalogue/Caméra/Capteur1.png"  class="imgborder" /></li>
-        <li><img src="../res/images/Catalogue/Caméra/Capteur2.png"  class="imgborder" /></li>
-    </ul>
-</div>
+    <div id="carrousel5">
+        <ul>
+            <li><img src="../res/images/Catalogue/Caméra/Capteur1.png"  class="imgborder" /></li>
+            <li><img src="../res/images/Catalogue/Caméra/Capteur2.png"  class="imgborder" /></li>
+        </ul>
+    </div>
 
-<script src="../src/js/Catalogue5.js"></script>
+    <script src="../src/js/Catalogue5.js"></script> -->
 
 </div>
 
