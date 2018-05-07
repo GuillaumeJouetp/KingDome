13bd875a009b363d79a0fc46f0621070e0a46354
Olfa <Olfa@DESKTOP-K59HCFE>
2018-05-07 09:46:38 +0200

    changement

--- src/php/views/dashboard2.php
+++ src/php/views/dashboard2.php
@@ -30,20 +30,69 @@
 	foreach($own_home as $donnees){ //boucle pour avoir le nombre de maison
 		
 		if($id == $donnees['user_id']){
-			$cpt++;
-			debug1($cpt);
+			$cpt++;	
 			echo(
 					"<div class='ecriture1'>"
 					.'<img class="fleche" src="..\res\icones\fleche_bas.png">'
 					."Maison $cpt"
 					."</div>"
-				);	
-		
+				);	?>
+				
+		<div id="conteneur">
+			<a href="#masque">
+				<div class="element , ecriture2">Ajouter un capteur</div>
+			</a>
+		   		
+			<div id="masque">
+			
+				<div class="fenetre-modale">
+					    <a class="fermer" href="#nullepart"><img src="..\res\icones\bouton-fermer.png"class="btn-fermer"/></a>
+					    <div class="capt"><br>Ajouter un capteur<br><br></div>
+							<form action="traitementFormulaire.php" method="post" >
+								
+								<label> Nom :  <br><br> <input type="text" name="myText"/> </label>  <br><br>
+									
+								
+							   		
+							   	<label> Pièce :  <br><br> </label>
+							   	<select class="custom-dropdown__select custom-dropdown__select--white">
+							     	<option>Pièce 1</option>
+							       	<option>Pièce 2</option>
+							   		<option>Pièce 3</option>
+								</select><br><br>
+							   		
+						   		<label> Type de capteur :  <br><br> </label>
+							   	<select class="custom-dropdown__select custom-dropdown__select--white">
+							   		<option>Capteur de température</option>
+						       		<option>Capteur d'humidité</option>
+						       		<option>Capteur de ...</option>
+						   		</select><br><br><br>
+
+							   	
+								<input type="submit" value="Ajouter" /><br><br>
+								
+							</form>
+								
+				</div> <!-- .fenetre-modale -->
+			</div> <!-- #masque -->
+		   		
+		   	
+	 		<div class="element">
+		   		<span class="custom-dropdown custom-dropdown--white">
+		    	    <select class="custom-dropdown__select custom-dropdown__select--white">
+				       	<option>Trier par : Pièce</option>
+				       	<option>Type</option>
+				       	<option>Favori</option>
+				   	</select>
+				</span>
+			</div>  
+			      
+	    </div>
+		<?php
 			
 			foreach($rooms as $donnees2){ //boucle pour avoir le nombre de piece
 				if($donnees['house_id'] == $donnees2['home_id']){
 					$cpt2++;
-					debug1($cpt2);
 					echo(
 							"<div class='pieces'>"
 							."<div class='piece'> Pièce $cpt2 </div>"
