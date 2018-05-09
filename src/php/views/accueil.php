c33635e41a26a03305b95fd9c95e865d26a91dfe
Nelly <Nelly@172.16.230.113>
2018-05-09 09:39:01 +0200

    Modification de la page d'accueil, pas finie

--- src/php/views/accueil.php
+++ src/php/views/accueil.php
@@ -1,10 +1,32 @@
+		<head>
+			<link rel="stylesheet" href="css/accueil.css">
+		</head>
+
 <div id="corps">
 
 	<button>
 		<a href="index.php?cible=consommation">
-			<img src="../res/icones/monCompte.png" alt="icone compte" class="icone">Consommation
+			<img src="../res/icones/consommation.png" alt="icone compte" class="icone">Consommation
 		</a>
     </button>
+    
+    <!-- Premiere section : nouveaute -->
+    <section id="nouveaute">
+    	<div id="nouveaute_L>
+    		<h1>Faites de votre maison un royaume !</h1>
+    	</div>
+    	<div id="nouveaute_R">
+    	    	<img src="../res/images/maison_accueil.jpg" id="img_accueil>
+    	</div>
+    </section>
+    
+    <!-- Deuxieme section : presentation -->
+    <section id="presentation">
+    	<h2>KingDome, qu'est ce que c'est ?</h2>
+    	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque pulvinar elementum dolor vitae luctus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut vel sollicitudin purus, vitae dapibus mauris. Sed finibus magna a varius sodales. Maecenas faucibus, arcu porttitor dignissim accumsan, nisi nibh maximus eros, ultricies cursus ipsum tellus porta nisi. Phasellus volutpat dignissim dignissim. Nullam sodales ornare nisl, sed sagittis risus euismod sed.</p>
+    </section>
+    
+    
     <p>test</p>
     <p>test</p>
     <p>test</p>
