d5cad83f92c40225ea33c8edcd55cdead5ebeb67
Guillaume <guillaume.jouet-pastre@hotmail.fr>
2018-05-15 10:38:01 +0200

    set up accueil admin

--- src/php/views/accueilAdmin.php
+++ src/php/views/accueilAdmin.php
@@ -1,14 +1,11 @@
-		<head>
-			<link rel="stylesheet" href="css/accueil.css">
-		</head>
+<head><link rel="stylesheet" href="css/accueil.css"></head>
+
 
 <div id="corps">
 
 	<p> Page accueilAdmin </p>
-	
-	
 	<!-- Graphique : nombres de connexions -->
-	    
-	    
-	     
+    <canvas id="myChart" width="400" height="200"></canvas>
+    <script type="text/javascript" src="js/graphiqueAdmin.js"></script>
+
 </div>
