13a28318b1ea4e98c5071dc6742221ffe7661ab0
Nelly <Nelly@172.16.230.113>
2018-05-15 09:11:45 +0200

    Pages accueilAdmin et accueilUser

---
+++ src/php/views/accueilAdmin.php
@@ -0,0 +1,14 @@
+		<head>
+			<link rel="stylesheet" href="css/accueil.css">
+		</head>
+
+<div id="corps">
+
+	<p> Page accueilAdmin </p>
+	    <!-- Graphique : nombres de connexions -->
+	    
+	    
+	    
+
+   
+</div>
---
+++ src/php/views/accueilUser.php
@@ -0,0 +1,9 @@
+		<head>
+			<link rel="stylesheet" href="css/accueil.css">
+		</head>
+
+<div id="corps">
+
+	<p> Page accueil User </p>
+   
+</div>
