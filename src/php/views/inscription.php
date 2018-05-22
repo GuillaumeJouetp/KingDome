617db849a5efd8267150c8158b125e54e01b4d2e
AlexMaio <alexandre.maiolini@gmail.com>
2018-05-22 09:46:26 +0200

    Reorganisation code formulaire incription (suppression p br etc.)

--- src/php/views/inscription.php
+++ src/php/views/inscription.php
@@ -22,60 +22,65 @@
 
             <form method="post" action="index.php?cible=utilisateur&function=inscription" enctype="multipart/form-data">
 
-                <p><label>Adresse mail*<br>
-                        <input type="email" name="email" size="39" required/></label></p>
+                <label>Adresse mail*<br>
+                        <input type="email" name="email" size="39" required/></label>
                     <?php
                     echo "<span class='Alerte_Message'>".$Email_Message."</span>"; ?>
 
-                <p><label>Mot de passe*<br>
-                        <input type="password" name="password" size="39" required/></label></p>
+                <label>Mot de passe*<br>
+                        <input type="password" name="password" size="39" required/></label>
 
-                <p><label>Confirmation du mot de passe*<br>
-                        <input type="password" name="password_confirmation" size="39" required/></label></p>
-                    <?php
-                    echo "<span class='Alerte_Message'>".$Password_Confirmation."</span><br>";
-                    echo "<span class='$Alerte_Password'>" ?> Le mot de passe doit comporter au moins 8 caractère, ainsi qu'au moins un chiffre et une majuscule.</span><br>
+                <label>Confirmation du mot de passe*<br>
+                        <input type="password" name="password_confirmation" size="39" required/>
+                </label>
+                <?php
+                    echo "<div class='Alerte_Message'>".$Password_Confirmation."</div>";
+                    echo "<span class='$Alerte_Password'>" ?> Le mot de passe doit comporter au moins 8 caractère, ainsi qu'au moins un chiffre et une majuscule.</span>
 
                 <h2>Mes informations personnelles</h2>
 
-                <p><label>Civilité *
+                <label>Civilité *
                         <input type="radio" name="civil" value="Mme" required checked id="civil_mme"/>Mme
-                        <input type="radio" name="civil" value="Mr" required id="civil_mr"/> Mr</label></p>
+                        <input type="radio" name="civil" value="Mr" required id="civil_mr"/> Mr
+                </label>
 
-                <p><label>Nom*<br>
-                    <input type="text" name="last_name" size="39" required/></label></p>
+                <label>Nom*<br>
+                    <input type="text" name="last_name" size="39" required/>
+                </label>
 
-                <p><label>Prénom*<br>
-                        <input type="text" namena="first_name" size="39" required/></label></p>
+                <label>Prénom*<br>
+                        <input type="text" name="first_name" size="39" required/>
+                </label>
 
-                <p>
-                    <label>Adresse*<br>
+
+                <label>Adresse*<br>
                     <input type="text" name="adress" id="autocomplete" size="39" placeholder="Entrez votre adresse"/>
                     </label>
-                    <input type="hidden" name="adress" id="fullAddr" size="39" disabled="true"/>
-                    <input type="hidden" id="street_number" disabled="true" />
-                    <input type="hidden" id="route" disabled="true" />
-                    <input type="hidden" id="country" disabled="true" />
-                    <input type="hidden" id="administrative_area_level_1" disabled="true" />
-                </p>
+                <input type="hidden" name="adress" id="fullAddr" size="39" disabled="true"/>
+                <input type="hidden" id="street_number" disabled="true" />
+                <input type="hidden" id="route" disabled="true" />
+                <input type="hidden" id="country" disabled="true" />
+                <input type="hidden" id="administrative_area_level_1" disabled="true" />
 
-                <p><label for="zip_code">Code postal*
+
+                <label for="zip_code" id="zip_label">Code postal*
                     <label for="ville" id="city">Ville*<br>
-                        <input type="text" name="zip_code" id="postal_code" size="14" disabled="true"/>
-                        <input type="text" name="city" id="locality" disabled="true"></label></label>
-                </p>
+                        <input type="text" name="zip_code" id="postal_code" size="13" disabled="true"/>
+                        <input type="text" name="city" id="locality" disabled="true">
+                    </label>
+                </label>
 
-                <p><label>Numéro de téléphone*<br>
-                    <input type="tel" name="tel" size="39" required/></label></p>
+                <label id="num_label">Numéro de téléphone*<br>
+                    <input type="tel" name="tel" size="39" required/>
+                </label>
                 <?php
                 echo "<span class='Alerte_Message'>".$Tel_Message."</span>"; ?>
 
-                <p>
-                    <label>Date de naissance<br>
-                        <input type="date" name="date_naissance" size="35"/></label>
+                <label id="birth_label">Date de naissance<br>
+                        <input type="date" name="date_naissance" size="35"/>
+                </label>
 
-                    <button type="button" id="myBtn" class="submit_button">Ajouter un avatar</button>
-                </p>
+                <button type="button" id="myBtn" class="submit_button">Ajouter un avatar</button>
                 <?php
                 echo "<span class='Alerte_Message'>".$Avatar_Message."</span>"; ?>
 
@@ -83,25 +88,25 @@
                 <div id="myModal" class="modal">
                         <div class="modal-content">
                             <span class="close">&times;</span>
-                            <p>
+
                                 <h3>Téléchargez votre avatar</h3>
                                 <h4>(JPG, PNG ou GIF)</h4><br>
                                 <input type="hidden" name="MAX_FILE_SIZE" value="250000" />
                                 <input type="file" name="avatar" size=50 class="avatar_button"/>
                                 <button type="button" class="submit_button" id="avatar_ok">Envoyer</button>
-                            </p>
+
                         </div>
                 </div>
 
-                <p>
-                    <label class="label_cgu">J'ai lu et approuvé les <a href="#" class="label_cgu">conditions générales d'utilisation</a>*
-                    <input type="checkbox" required id="cgu"></label>
-                </p>
 
-                <p class="submit">
+                <label id="cgu_label">J'ai lu et approuvé les <a href="#" id="cgu_link">conditions générales d'utilisation*</a>
+                        <input type="checkbox" required id="cgu_checkbox">
+                </label>
+
+                <div class="submit">
                     <button type="submit" name="creation_submit" class="submit_button">S'inscrire</button>
                     <button type="reset" name="reset" class="submit_button" id="reset_button">Annuler</button>
-                </p>
+                </div>
                 <br>* Champs requis
 
             </form>
@@ -111,15 +116,19 @@
             <h1>Déjà inscrit?</h1><br><br>
 
             <form method="POST" action="index.php?cible=utilisateur&function=connexion">
-            <p><label>Mon adresse mail*<br>
-                    <input type="email" name="email" size="39" required/></label></p>
 
-            <p><label>Mon mot de passe*<br>
-                    <input type="password" name="password" size="39" required/></label></p>
+                <label>Mon adresse mail*<br>
+                    <input type="email" name="email" size="39" required/>
+                </label>
 
-            <p class="connexion_button">
+            <label>Mon mot de passe*<br>
+                    <input type="password" name="password" size="39" required/>
+            </label>
+
+            <div class="connexion_button">
                 <button type="submit" name="connexion_submit" class="submit_button">Connexion</button>
-            </p>
+            </div>
+
                 <p>* Champs requis</p>
 
             </form>
