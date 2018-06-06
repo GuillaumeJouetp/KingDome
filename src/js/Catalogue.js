f005a391080f523cfcdaacf62363cd83e3497622
LucasAPP92 <lucaslemal@gmail.com>
2018-06-06 11:21:53 +0200

    Catalogue

--- src/js/Catalogue.js
+++ src/js/Catalogue.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
-    var $carrousel = $('#carrousel'),
-        $img = $('#carrousel img'),
+    var $carrousel = $('#carrousel1'),
+        $img = $('#carrousel1 img'),
         indexImg = $img.length - 1,
         i = 0,
         $currentImg = $img.eq(i);
@@ -9,43 +9,541 @@ $(document).ready(function() {
     $img.css('display', 'none');
     $currentImg.css('display', 'block');
 
-    $carrousel.append('<div class="controls"><span class="prev"><img src="../res/icones/arrow-point-to-left.png"></span><span class="next"><img src="../res/icones/arrow-point-to-right.png"></span></div>');
+    if ($img.length > 1) {
 
-    $('.next').click(function () {
+        $carrousel.append('<div class="controls"> <span class="prev1"><img src="../res/icones/arrow-point-to-left.png"></span> <span class="next1"><img src="../res/icones/arrow-point-to-right.png"></span> </div>');
 
-        i++;
+        $('.next1').click(function () {
 
-        if (i <= indexImg) {
-            $img.css('display', 'none');
-            $currentImg = $img.eq(i);
-            $currentImg.css('display', 'block');
-        }
-        else {
-            i = 0;
-            $img.css('display', 'none');
-            $currentImg = $img.eq(i);
-            $currentImg.css('display', 'block');
-        }
+            i++;
 
-    });
+            if (i <= indexImg) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = 0;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
 
-    $('.prev').click(function () {
+        });
 
-        i--;
+        $('.prev1').click(function () {
 
-        if (i >= 0) {
-            $img.css('display', 'none');
-            $currentImg = $img.eq(i);
-            $currentImg.css('display', 'block');
-        }
-        else {
-            i = indexImg;
-            $img.css('display', 'none');
-            $currentImg = $img.eq(i);
-            $currentImg.css('display', 'block');
-        }
+            i--;
 
-    });
+            if (i >= 0) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = indexImg;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
 
+        });
+
+    }
+
+});
+
+$(document).ready(function() {
+
+    var $carrousel = $('#carrousel2'),
+        $img = $('#carrousel2 img'),
+        indexImg = $img.length - 1,
+        i = 0,
+        $currentImg = $img.eq(i);
+
+    $img.css('display', 'none');
+    $currentImg.css('display', 'block');
+
+    if ($img.length > 1) {
+
+        $carrousel.append('<div class="controls"> <span class="prev2"><img src="../res/icones/arrow-point-to-left.png"></span> <span class="next2"><img src="../res/icones/arrow-point-to-right.png"></span> </div>');
+
+        $('.next2').click(function () {
+
+            i++;
+
+            if (i <= indexImg) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = 0;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+        $('.prev2').click(function () {
+
+            i--;
+
+            if (i >= 0) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = indexImg;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+    }
+
+});
+
+$(document).ready(function() {
+
+    var $carrousel = $('#carrousel3'),
+        $img = $('#carrousel3 img'),
+        indexImg = $img.length - 1,
+        i = 0,
+        $currentImg = $img.eq(i);
+
+    $img.css('display', 'none');
+    $currentImg.css('display', 'block');
+
+    if ($img.length > 1) {
+
+        $carrousel.append('<div class="controls"> <span class="prev3"><img src="../res/icones/arrow-point-to-left.png"></span> <span class="next3"><img src="../res/icones/arrow-point-to-right.png"></span> </div>');
+
+        $('.next3').click(function () {
+
+            i++;
+
+            if (i <= indexImg) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = 0;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+        $('.prev3').click(function () {
+
+            i--;
+
+            if (i >= 0) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = indexImg;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+    }
+
+})
+
+$(document).ready(function() {
+
+    var $carrousel = $('#carrousel4'),
+        $img = $('#carrousel4 img'),
+        indexImg = $img.length - 1,
+        i = 0,
+        $currentImg = $img.eq(i);
+
+    $img.css('display', 'none');
+    $currentImg.css('display', 'block');
+
+    if ($img.length > 1) {
+
+        $carrousel.append('<div class="controls"> <span class="prev4"><img src="../res/icones/arrow-point-to-left.png"></span> <span class="next4"><img src="../res/icones/arrow-point-to-right.png"></span> </div>');
+
+        $('.next4').click(function () {
+
+            i++;
+
+            if (i <= indexImg) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = 0;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+        $('.prev4').click(function () {
+
+            i--;
+
+            if (i >= 0) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = indexImg;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+    }
+
+});
+
+$(document).ready(function() {
+
+    var $carrousel = $('#carrousel5'),
+        $img = $('#carrousel5 img'),
+        indexImg = $img.length - 1,
+        i = 0,
+        $currentImg = $img.eq(i);
+
+    $img.css('display', 'none');
+    $currentImg.css('display', 'block');
+
+    if ($img.length > 1) {
+
+        $carrousel.append('<div class="controls"> <span class="prev5"><img src="../res/icones/arrow-point-to-left.png"></span> <span class="next5"><img src="../res/icones/arrow-point-to-right.png"></span> </div>');
+
+        $('.next5').click(function () {
+
+            i++;
+
+            if (i <= indexImg) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = 0;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+        $('.prev5').click(function () {
+
+            i--;
+
+            if (i >= 0) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = indexImg;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+    }
+
+});
+
+$(document).ready(function() {
+
+    var $carrousel = $('#carrousel6'),
+        $img = $('#carrousel6 img'),
+        indexImg = $img.length - 1,
+        i = 0,
+        $currentImg = $img.eq(i);
+
+    $img.css('display', 'none');
+    $currentImg.css('display', 'block');
+
+    if ($img.length > 1) {
+
+        $carrousel.append('<div class="controls"> <span class="prev6"><img src="../res/icones/arrow-point-to-left.png"></span> <span class="next6"><img src="../res/icones/arrow-point-to-right.png"></span> </div>');
+
+        $('.next6').click(function () {
+
+            i++;
+
+            if (i <= indexImg) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = 0;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+        $('.prev6').click(function () {
+
+            i--;
+
+            if (i >= 0) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = indexImg;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+    }
+
+});
+
+$(document).ready(function() {
+
+    var $carrousel = $('#carrousel7'),
+        $img = $('#carrousel7 img'),
+        indexImg = $img.length - 1,
+        i = 0,
+        $currentImg = $img.eq(i);
+
+    $img.css('display', 'none');
+    $currentImg.css('display', 'block');
+
+    if ($img.length > 1) {
+
+        $carrousel.append('<div class="controls"> <span class="prev7"><img src="../res/icones/arrow-point-to-left.png"></span> <span class="next7"><img src="../res/icones/arrow-point-to-right.png"></span> </div>');
+
+        $('.next7').click(function () {
+
+            i++;
+
+            if (i <= indexImg) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = 0;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+        $('.prev7').click(function () {
+
+            i--;
+
+            if (i >= 0) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = indexImg;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+    }
+
+});
+
+$(document).ready(function() {
+
+    var $carrousel = $('#carrousel8'),
+        $img = $('#carrousel8 img'),
+        indexImg = $img.length - 1,
+        i = 0,
+        $currentImg = $img.eq(i);
+
+    $img.css('display', 'none');
+    $currentImg.css('display', 'block');
+
+    if ($img.length > 1) {
+
+        $carrousel.append('<div class="controls"> <span class="prev8"><img src="../res/icones/arrow-point-to-left.png"></span> <span class="next8"><img src="../res/icones/arrow-point-to-right.png"></span> </div>');
+
+        $('.next8').click(function () {
+
+            i++;
+
+            if (i <= indexImg) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = 0;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+        $('.prev8').click(function () {
+
+            i--;
+
+            if (i >= 0) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = indexImg;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+    }
+
+});
+
+$(document).ready(function() {
+
+    var $carrousel = $('#carrousel9'),
+        $img = $('#carrousel9 img'),
+        indexImg = $img.length - 1,
+        i = 0,
+        $currentImg = $img.eq(i);
+
+    $img.css('display', 'none');
+    $currentImg.css('display', 'block');
+
+    if ($img.length > 1) {
+
+        $carrousel.append('<div class="controls"> <span class="prev9"><img src="../res/icones/arrow-point-to-left.png"></span> <span class="next9"><img src="../res/icones/arrow-point-to-right.png"></span> </div>');
+
+        $('.next9').click(function () {
+
+            i++;
+
+            if (i <= indexImg) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = 0;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+        $('.prev9').click(function () {
+
+            i--;
+
+            if (i >= 0) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = indexImg;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+    }
+
+});
+
+$(document).ready(function() {
+
+    var $carrousel = $('#carrousel10'),
+        $img = $('#carrousel10 img'),
+        indexImg = $img.length - 1,
+        i = 0,
+        $currentImg = $img.eq(i);
+
+    $img.css('display', 'none');
+    $currentImg.css('display', 'block');
+
+    if ($img.length > 1) {
+
+        $carrousel.append('<div class="controls"> <span class="prev10"><img src="../res/icones/arrow-point-to-left.png"></span> <span class="next10"><img src="../res/icones/arrow-point-to-right.png"></span> </div>');
+
+        $('.next10').click(function () {
+
+            i++;
+
+            if (i <= indexImg) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = 0;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+        $('.prev10').click(function () {
+
+            i--;
+
+            if (i >= 0) {
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+            else {
+                i = indexImg;
+                $img.css('display', 'none');
+                $currentImg = $img.eq(i);
+                $currentImg.css('display', 'block');
+            }
+
+        });
+
+    }
 
 });
--- src/php/controllers/catalogue.php
+++ src/php/controllers/catalogue.php
@@ -12,12 +12,44 @@ if( ! isAnAdmin($bdd)) {
             $vue = "catalogue_backoffice";
             break;
         case 'modifier_texte':
-            $req = $bdd->prepare('UPDATE updatable_content SET content= :nouveau_texte WHERE id = 1');
-            $req->execute(array(
-                'nouveau_texte' => $_POST['texte_catalogue']
-            ));
+            $req = $bdd->prepare('UPDATE updatable_content SET content= :nouveau_texte, date= :nouvelle_date WHERE id = 1');
+            $req->execute(array('nouveau_texte' => $_POST['texte_catalogue'], 'nouvelle_date' => date("Y-m-d")));
             $vue = "catalogue_backoffice";
             break;
+        case 'ajouter_type':
+            $req = $bdd->prepare('INSERT INTO device_types(name) VALUES( :nom)');
+            $req->execute(array('nom' => $_POST['nom_type']));
+            $vue = "catalogue_backoffice";
+            break;
+        case 'supprimer_type':
+            $req1 = $bdd->prepare('DELETE FROM device_types WHERE name= :nom_type');
+            $req1->execute(array('nom_type' => $_POST['type_capteur_catalogue']));
+            $req2 = $bdd->prepare('DELETE FROM catalog WHERE device_type_name= :nom_type');
+            $req2->execute(array('nom_type' => $_POST['type_capteur_catalogue']));
+            $vue = "catalogue_backoffice";
+            break;
+        case 'ajouter_capteur':
+            $req = $bdd->prepare('INSERT INTO catalog(url, device_type_name, name) VALUES( :lien_image, :nom_type, :nom_capteur)');
+
+            if(isset($_FILES['file_image_catalogue'])) {
+                $Avatar_Message = isAnAvatar($_FILES['file_image_catalogue']['name'], $_FILES['file_image_catalogue']['size'], $_FILES['file_image_catalogue']['tmp_name'], $_FILES['file_image_catalogue']['error']);
+                $dir = '../res/images/Catalogue/';
+                $ext = strtolower(pathinfo($_FILES['file_image_catalogue']['name'],PATHINFO_EXTENSION));
+                $file = $_POST['type_capteur_catalogue'] . '_' . uniqid().'.'.$ext;
+                $img = $dir.$file;
+                move_uploaded_file($_FILES['file_image_catalogue']['tmp_name'], $img);
+            }
+
+            $req->execute(array('lien_image' => $img, 'nom_type' => $_POST['type_capteur_catalogue'], 'nom_capteur' => $_POST['nom_capteur']));
+            $vue = "catalogue_backoffice";
+            break;
+        case 'supprimer_capteur':
+            $req = $bdd->prepare('DELETE FROM catalog WHERE name = :nom_capteur');
+            $req->execute(array('nom_capteur' => $_POST['nom_capteur_catalogue']));
+            $vue = "catalogue_backoffice";
+            break;
+
+
     }
 }
 
