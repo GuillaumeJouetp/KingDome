9efc7e527e870c1a0f5d859ee82aba90f366d838
Adrien <adrien.jeanclos@gmail.com>
2018-06-24 23:20:12 +0200

    monCompte

--- src/php/views/monCompte.php
+++ src/php/views/monCompte.php
@@ -6,7 +6,6 @@
  */
 ?>
 
-<!DOCTYPE html>
 
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,7 +14,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
-<body>
 <div id="corps">
 
     <div id="profil">
@@ -302,4 +300,3 @@
     </p>
 
 </div>
-</body>
